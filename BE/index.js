c2d92b7d040a6c3fb8d9ccf822a028e3ee241c59
Hai Bui <haibui.cs@gmail.com>
2022-12-03 19:59:41 +0700

    new package cors

--- BE/index.js
+++ BE/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const usersRouter = require("./src/routes/usersRoute");
 const mcpsRouter = require("./src/routes/mcpRoute");
 const port = 1337;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", usersRouter);
 app.use("/api", mcpsRouter);
 
