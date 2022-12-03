61f525e95595a49e3c9eb1fea23b37532080256b
Hai Bui <haibui.cs@gmail.com>
2022-12-04 02:25:53 +0700

    fix mcps api

--- BE/src/controllers/mcpController.js
+++ BE/src/controllers/mcpController.js
@@ -1,6 +1,5 @@
 const dbo = require("../db/config");
 
-
 const db = dbo.getDb();
 
 exports.findAllMcps = async (req, res) => {
@@ -10,16 +9,11 @@ exports.findAllMcps = async (req, res) => {
 
 exports.findAllOverloadedMcps = async (req, res) => {
   const allMCPs = await db.collection("MCP").find({}).toArray();
-  const allOverloadedMCPs = allUsers.filter((mcp) => mcp.Status == "Overloaded");
-  return res.send(allWorkers);
+  const allOverloadedMCPs = allMCPs.filter((mcp) => mcp.Status == "Overloaded");
+  return res.send(allOverloadedMCPs);
 };
 
 exports.findMcp = async (req, res) => {
   const MCP = await db.collection("MCP").findOne({ MCP_ID: req.params.id });
   return res.send(MCP);
 };
-
-
-
- 
-
